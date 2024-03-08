9e386da01b4aa404d321be9edc685472db476c68
Rajat Sharma <36663311+iamRajatSharma@users.noreply.github.com>
2024-03-08 13:28:14 +0530

    Update First.js

--- First.js
+++ First.js
@@ -1,15 +1,1 @@
-console.log('First file');
-
-console.log('Second line from First file');
-
-
-console.log("Another file added");
-console.log("Another file added");
-console.log("Another file added");
-console.log("Another file added");
-console.log("Another file added");
-console.log("Another file added");
-
-
-console.log("Another file added");
-console.log("Another file added");
+console.log("cleaned file")
